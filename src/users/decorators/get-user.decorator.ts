49282ad5c55e66d78d53828d787b88bbfcb4b3eb
Vladlen <ilusenkovladlen@gmail.com>
2022-12-30 20:27:33 +0200

    fix: fixed GetUser decorator

--- src/users/decorators/get-user.decorator.ts
+++ src/users/decorators/get-user.decorator.ts
@@ -1,6 +1,9 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 
-export const GetUser = createParamDecorator((data, req): User => {
-  return req.user;
-});
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+  },
+);
