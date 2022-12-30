75857d4ac7ae26fb7b9a1c6f3c80db225cbdf364
Vladlen <ilusenkovladlen@gmail.com>
2022-12-30 16:35:15 +0200

    feat: implemented apiKeyGuard

---
+++ src/users/guards/api-key.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserRepository } from '../repositories/user.repository';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(private userRepository: UserRepository) {}
+  validateKey(apiKey: string) {
+    const user = this.userRepository.findOneBy({ apiKey });
+    return user !== null;
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    return this.validateKey(req.headers['api-key']);
+  }
+}
