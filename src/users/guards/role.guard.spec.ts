b4e321130d063842691aa727363cdd5f2db35da2
Vladlen <ilusenkovladlen@gmail.com>
2022-12-30 15:28:02 +0200

    fix: fixed role.guard.spec.ts

--- src/users/guards/role.guard.spec.ts
+++ src/users/guards/role.guard.spec.ts
@@ -1,7 +1,16 @@
 import { RoleGuard } from './role.guard';
+import { Reflector } from '@nestjs/core';
 
 describe('RoleGuard', () => {
+  let roleGuard: RoleGuard;
+  let reflector: Reflector;
+
+  beforeEach(async () => {
+    reflector = new Reflector();
+    roleGuard = new RoleGuard(reflector);
+  });
+
   it('should be defined', () => {
-    expect(new RoleGuard()).toBeDefined();
+    expect(roleGuard).toBeDefined();
   });
 });
