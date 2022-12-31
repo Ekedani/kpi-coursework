dfb0d1168c8f1d58b5966317481138d1f2fd2e97
Vladlen <ilusenkovladlen@gmail.com>
2022-12-31 17:02:51 +0200

    feat: added KinopoiskGenresDictionary

---
+++ src/media/common/dictionaries/kinopoisk-genres.dictionary.ts
@@ -0,0 +1,167 @@
+export const KinopoiskGenresDictionary = [
+  {
+    id: 1,
+    genreRu: 'триллер',
+    genreEn: 'thriller',
+  },
+  {
+    id: 2,
+    genreRu: 'драма',
+    genreEn: 'drama',
+  },
+  {
+    id: 3,
+    genreRu: 'криминал',
+    genreEn: 'crime',
+  },
+  {
+    id: 4,
+    genreRu: 'мелодрама',
+    genreEn: 'melodrama',
+  },
+  {
+    id: 5,
+    genreRu: 'детектив',
+    genreEn: 'detective',
+  },
+  {
+    id: 6,
+    genreRu: 'фантастика',
+    genreEn: 'fantastic',
+  },
+  {
+    id: 7,
+    genreRu: 'приключения',
+    genreEn: 'adventure',
+  },
+  {
+    id: 8,
+    genreRu: 'биография',
+    genreEn: 'biography',
+  },
+  {
+    id: 9,
+    genreRu: 'фильм-нуар',
+    genreEn: 'noir',
+  },
+  {
+    id: 10,
+    genreRu: 'вестерн',
+    genreEn: 'western',
+  },
+  {
+    id: 11,
+    genreRu: 'боевик',
+    genreEn: 'action',
+  },
+  {
+    id: 12,
+    genreRu: 'фэнтези',
+    genreEn: 'fantasy',
+  },
+  {
+    id: 13,
+    genreRu: 'комедия',
+    genreEn: 'comedy',
+  },
+  {
+    id: 14,
+    genreRu: 'военный',
+    genreEn: 'military',
+  },
+  {
+    id: 15,
+    genreRu: 'история',
+    genreEn: 'history',
+  },
+  {
+    id: 16,
+    genreRu: 'музыка',
+    genreEn: 'music',
+  },
+  {
+    id: 17,
+    genreRu: 'ужасы',
+    genreEn: 'horror',
+  },
+  {
+    id: 18,
+    genreRu: 'мультфильм',
+    genreEn: 'cartoon',
+  },
+  {
+    id: 19,
+    genreRu: 'семейный',
+    genreEn: 'family',
+  },
+  {
+    id: 20,
+    genreRu: 'мюзикл',
+    genreEn: 'musical',
+  },
+  {
+    id: 21,
+    genreRu: 'спорт',
+    genreEn: 'sport',
+  },
+  {
+    id: 22,
+    genreRu: 'документальный',
+    genreEn: 'documentary',
+  },
+  {
+    id: 23,
+    genreRu: 'короткометражка',
+    genreEn: 'short',
+  },
+  {
+    id: 24,
+    genreRu: 'аниме',
+    genreEn: 'anime',
+  },
+  {
+    id: 25,
+    genreRu: '',
+    genreEn: '',
+  },
+  {
+    id: 26,
+    genreRu: 'новости',
+    genreEn: 'news',
+  },
+  {
+    id: 27,
+    genreRu: 'концерт',
+    genreEn: 'concert',
+  },
+  {
+    id: 28,
+    genreRu: 'для взрослых',
+    genreEn: 'adult',
+  },
+  {
+    id: 29,
+    genreRu: 'церемония',
+    genreEn: 'ceremony',
+  },
+  {
+    id: 30,
+    genreRu: 'реальное ТВ',
+    genreEn: 'real TV',
+  },
+  {
+    id: 31,
+    genreRu: 'игра',
+    genreEn: 'game',
+  },
+  {
+    id: 32,
+    genreRu: 'ток-шоу',
+    genreEn: 'talk show',
+  },
+  {
+    id: 33,
+    genreRu: 'детский',
+    genreEn: 'childish',
+  },
+];
