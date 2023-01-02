e7e0b5b7bfdec9363ef2b71863454e337d957b43
Vladlen <ilusenkovladlen@gmail.com>
2023-01-02 17:26:58 +0200

    feat: expanded MediaInterface

--- src/media/common/media.interface.ts
+++ src/media/common/media.interface.ts
@@ -1,5 +1,12 @@
 export interface MediaInterface {
   sources: Array<string>;
-  name: string;
-  links: Array<string>;
+  nameOriginal: string;
+  year: number;
+  rating: {
+    [key: string]: number;
+  };
+  images: Array<string>;
+  links: {
+    [key: string]: string;
+  };
 }
