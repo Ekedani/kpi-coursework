4b88f89221fb3f3bcad2a75a704cf64e4c1b6d47
Vladlen <ilusenkovladlen@gmail.com>
2023-01-02 15:29:40 +0200

    fix: removed redundant error handling from users module

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  Injectable,
-  InternalServerErrorException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { SignUpCredentialsDto } from '../dto/sign-up-credentials.dto';
 import { SignInCredentialsDto } from '../dto/sign-in-credentials.dto';
@@ -21,57 +16,41 @@ export class AuthService {
 
   async signUp(signUpCredentialsDto: SignUpCredentialsDto) {
     const { firstName, lastName, email, password } = signUpCredentialsDto;
-    try {
-      const salt = await bcrypt.genSalt();
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const newUser = await this.userRepository.save({
-        firstName,
-        lastName,
-        email,
-        role: UserRole.User,
-        password: hashedPassword,
-      });
-      const payload: JwtPayload = {
-        id: newUser.id,
-        firstName: newUser.firstName,
-        lastName: newUser.lastName,
-        email: newUser.email,
-        role: newUser.role,
-      };
-      const accessToken = this.jwtService.sign(payload);
-      return { accessToken };
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await this.userRepository.save({
+      firstName,
+      lastName,
+      email,
+      role: UserRole.User,
+      password: hashedPassword,
+    });
+    const payload: JwtPayload = {
+      id: newUser.id,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      email: newUser.email,
+      role: newUser.role,
+    };
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken };
   }
 
   async signIn(signInCredentialsDto: SignInCredentialsDto) {
     const { email, password } = signInCredentialsDto;
-    try {
-      const user = await this.userRepository.findOneBy({ email });
-      if (user && (await bcrypt.compare(password, user.password))) {
-        const payload: JwtPayload = {
-          id: user.id,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          role: user.role,
-        };
-        const accessToken = this.jwtService.sign(payload);
-        return { accessToken };
-      } else {
-        throw new UnauthorizedException();
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const user = await this.userRepository.findOneBy({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+      };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException();
     }
   }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -13,75 +13,43 @@ export class UsersService {
   constructor(private userRepository: UserRepository) {}
 
   async findAll(findUsersDto: FindUsersDto) {
-    try {
-      const users = await this.userRepository.findAll(findUsersDto);
-      users.data.forEach((user) => delete user.password);
-      return { data: users.data, total: users.total, pages: users.pages };
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
+    const users = await this.userRepository.findAll(findUsersDto);
+    users.data.forEach((user) => delete user.password);
+    return { data: users.data, total: users.total, pages: users.pages };
   }
 
   async findOne(id: string) {
-    try {
-      const user = await this.userRepository.findOneBy({ id });
-      if (!user) {
-        throw new NotFoundException();
-      } else {
-        return {
-          id: user.id,
-          apiKey: user.apiKey,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          role: user.role,
-          email: user.email,
-        };
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException();
+    } else {
+      return {
+        id: user.id,
+        apiKey: user.apiKey,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+        email: user.email,
+      };
     }
   }
 
   async remove(id: string) {
-    try {
-      const result = await this.userRepository.delete({ id });
-      if (result.affected === 0) {
-        throw new NotFoundException();
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const result = await this.userRepository.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException();
     }
   }
 
   async generateApiKey(id: string) {
-    try {
-      const newKey = randomUUID();
-      const user = await this.userRepository.findOneBy({ id });
-      if (!user) {
-        throw new NotFoundException();
-      } else {
-        user.apiKey = newKey;
-        await this.userRepository.save(user);
-        return { apiKey: newKey };
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const newKey = randomUUID();
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException();
+    } else {
+      user.apiKey = newKey;
+      await this.userRepository.save(user);
+      return { apiKey: newKey };
     }
   }
 }
