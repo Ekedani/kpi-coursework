76f059d879b0427899d5ebc3b171e23d9a07d1f2
Vladlen <ilusenkovladlen@gmail.com>
2023-01-02 15:17:11 +0200

    fix: removed redundant error handling & fixed patch cinema

--- src/cinemas/services/cinemas.service.ts
+++ src/cinemas/services/cinemas.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCinemaDto } from '../dto/create-cinema.dto';
 import { UpdateCinemaDto } from '../dto/update-cinema.dto';
 import { CinemaRepository } from '../repositories/cinema.repository';
@@ -15,116 +10,67 @@ export class CinemasService {
   constructor(private cinemaRepository: CinemaRepository) {}
 
   async create(picture, createCinemaDto: CreateCinemaDto) {
-    try {
-      return await this.cinemaRepository.save({
-        name: createCinemaDto.name,
-        description: createCinemaDto.description,
-        picture: picture?.filename,
-        link: createCinemaDto.link,
-      });
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
+    return await this.cinemaRepository.save({
+      name: createCinemaDto.name,
+      description: createCinemaDto.description,
+      picture: picture?.filename,
+      link: createCinemaDto.link,
+    });
   }
 
   async findAll(findCinemaDto: FindCinemasDto) {
-    try {
-      const cinemas = await this.cinemaRepository.findAll(findCinemaDto);
-      cinemas.data.forEach((cinema) => {
-        if (!cinema.picture) {
-          delete cinema.picture;
-        }
-      });
-      return { data: cinemas.data, total: cinemas.total, pages: cinemas.pages };
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
+    const cinemas = await this.cinemaRepository.findAll(findCinemaDto);
+    cinemas.data.forEach((cinema) => {
+      if (!cinema.picture) {
+        delete cinema.picture;
       }
-    }
+    });
+    return { data: cinemas.data, total: cinemas.total, pages: cinemas.pages };
   }
 
   async findOne(id: string) {
-    try {
-      const cinema = await this.cinemaRepository.findOneBy({ id });
-      if (!cinema) {
-        throw new NotFoundException();
-      } else {
-        if (!cinema.picture) {
-          delete cinema.picture;
-        }
-        return cinema;
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const cinema = await this.cinemaRepository.findOneBy({ id });
+    if (!cinema) {
+      throw new NotFoundException();
     }
+    if (!cinema.picture) {
+      delete cinema.picture;
+    }
+    return cinema;
   }
 
   async update(id: string, picture, updateCinemaDto: UpdateCinemaDto) {
-    try {
-      const cinema = await this.cinemaRepository.findOneBy({ id });
-      if (!cinema) {
-        throw new NotFoundException();
-      }
-      if (picture) {
-        unlinkSync(`./uploads/${cinema.picture}`);
-      }
-      return await this.cinemaRepository.save({
-        id: cinema.id,
-        ...updateCinemaDto,
-        picture: picture?.filename,
-      });
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const cinema = await this.cinemaRepository.findOneBy({ id });
+    if (!cinema) {
+      throw new NotFoundException();
     }
+    if (picture && cinema.picture) {
+      unlinkSync(`./uploads/${cinema.picture}`);
+    }
+    return await this.cinemaRepository.save({
+      id: cinema.id,
+      ...updateCinemaDto,
+      picture: picture?.filename,
+    });
   }
 
   async remove(id: string) {
-    try {
-      const cinema = await this.cinemaRepository.findOneBy({ id });
-      if (!cinema) {
-        throw new NotFoundException();
-      }
-      if (cinema.picture) {
-        unlinkSync(`./uploads/${cinema.picture}`);
-      }
-      await this.cinemaRepository.delete({ id });
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const cinema = await this.cinemaRepository.findOneBy({ id });
+    if (!cinema) {
+      throw new NotFoundException();
     }
+    if (cinema.picture) {
+      unlinkSync(`./uploads/${cinema.picture}`);
+    }
+    await this.cinemaRepository.delete({ id });
   }
 
   async findPicture(id: string) {
-    try {
-      const path = (await this.cinemaRepository.findOneBy({ id })).picture;
-      if (!path) {
-        throw new NotFoundException();
-      } else {
-        return path;
-      }
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      } else {
-        throw new InternalServerErrorException();
-      }
+    const path = (await this.cinemaRepository.findOneBy({ id })).picture;
+    if (!path) {
+      throw new NotFoundException();
+    } else {
+      return path;
     }
   }
 }
